6b9be1eaf47d3da2a83c86c7a08002c7808e71ba
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-05 16:03:48 +0500

    initial commit

--- movieAPI/src/main/java/io/fullstack/movieAPI/MovieApiApplication.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/MovieApiApplication.java
@@ -2,12 +2,14 @@ package io.fullstack.movieAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
 	}
-
 }
