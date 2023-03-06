f0099f77772c1d96950b277695fd75ddecdf1b18
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-06 21:12:52 +0500

    feat: writing controller class

--- movieAPI/src/main/java/io/fullstack/movieAPI/controller/MovieController.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/controller/MovieController.java
@@ -1,2 +1,26 @@
-package io.fullstack.movieAPI.controller;public class MovieController {
+package io.fullstack.movieAPI.controller;
+
+import io.fullstack.movieAPI.model.Movie;
+import io.fullstack.movieAPI.service.MoviesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/movies/")
+public class MovieController {
+    private final MoviesService moviesService;
+    @GetMapping("getAllMovies")
+    public ResponseEntity<List<Movie>> allMovies(){
+        return new ResponseEntity<List<Movie>>(moviesService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @PostMapping("saveMovie")
+    public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie){
+        return new ResponseEntity<Movie>(moviesService.saveMovie(movie),HttpStatus.OK);
+    }
 }
