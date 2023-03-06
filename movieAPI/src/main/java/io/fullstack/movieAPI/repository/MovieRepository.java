823021d28beecf7e438f1e272bf68ee05b38fea7
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-06 21:14:40 +0500

    feat: Movie Repository using MongoRepository

--- movieAPI/src/main/java/io/fullstack/movieAPI/repository/MovieRepository.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/repository/MovieRepository.java
@@ -1,2 +1,10 @@
-package io.fullstack.movieAPI.repository;public class MovieRepository {
+package io.fullstack.movieAPI.repository;
+
+import io.fullstack.movieAPI.model.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 }
