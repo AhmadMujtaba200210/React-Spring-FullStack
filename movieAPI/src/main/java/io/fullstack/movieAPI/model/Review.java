8d16255802f5051e96d33c6e119b71d7b2ebecbe
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-06 21:14:15 +0500

    build: making models

--- movieAPI/src/main/java/io/fullstack/movieAPI/model/Review.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/model/Review.java
@@ -1,2 +1,18 @@
-package io.fullstack.movieAPI.model;public class Review {
+package io.fullstack.movieAPI.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document (collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
 }
