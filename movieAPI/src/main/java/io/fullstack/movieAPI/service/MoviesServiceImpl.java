2d9c467851314d999abcf096d918e093ec17fabc
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-11 17:59:25 +0500

    feat: business logic of getting single movie

--- movieAPI/src/main/java/io/fullstack/movieAPI/service/MoviesServiceImpl.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/service/MoviesServiceImpl.java
@@ -3,14 +3,18 @@ package io.fullstack.movieAPI.service;
 import io.fullstack.movieAPI.model.Movie;
 import io.fullstack.movieAPI.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MoviesServiceImpl implements MoviesService{
+
     private final MovieRepository movieRepository;
+
     @Override
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
@@ -19,6 +23,11 @@ public class MoviesServiceImpl implements MoviesService{
     @Override
     public Movie saveMovie(Movie movie) {
         return movieRepository.save(movie);
+    }
+
+    @Override
+    public Optional<Movie> getMovie(ObjectId movieName) {
+        return movieRepository.findById(movieName);
     }
 
 }
