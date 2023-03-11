f0c2daf3e83452440c05917112a22fbd5ec171fb
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-11 17:58:55 +0500

    feat: interface method for getting single movie

--- movieAPI/src/main/java/io/fullstack/movieAPI/service/MoviesService.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/service/MoviesService.java
@@ -1,11 +1,14 @@
 package io.fullstack.movieAPI.service;
 
 import io.fullstack.movieAPI.model.Movie;
+import org.bson.types.ObjectId;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MoviesService {
     List<Movie> getAllMovies();
     Movie saveMovie(Movie movie);
+    Optional<Movie> getMovie(ObjectId movieId);
 
 }
