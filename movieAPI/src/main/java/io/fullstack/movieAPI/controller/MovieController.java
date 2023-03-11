0b96615365696a98a991e1e8903ca624bc66570a
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-03-11 17:58:06 +0500

    feat: new controller method for getting single movie

--- movieAPI/src/main/java/io/fullstack/movieAPI/controller/MovieController.java
+++ movieAPI/src/main/java/io/fullstack/movieAPI/controller/MovieController.java
@@ -3,11 +3,13 @@ package io.fullstack.movieAPI.controller;
 import io.fullstack.movieAPI.model.Movie;
 import io.fullstack.movieAPI.service.MoviesService;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +18,18 @@ public class MovieController {
     private final MoviesService moviesService;
     @GetMapping("getAllMovies")
     public ResponseEntity<List<Movie>> allMovies(){
-        return new ResponseEntity<List<Movie>>(moviesService.getAllMovies(), HttpStatus.OK);
+        return new ResponseEntity<>(moviesService.getAllMovies(), HttpStatus.OK);
     }
 
     @PostMapping("saveMovie")
     public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie){
-        return new ResponseEntity<Movie>(moviesService.saveMovie(movie),HttpStatus.OK);
+        return new ResponseEntity<>(moviesService.saveMovie(movie), HttpStatus.OK);
+    }
+
+    @GetMapping("getMovie/{objectId}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable ObjectId objectId){
+        return new ResponseEntity<>(this.moviesService.getMovie(objectId), HttpStatus.OK);
     }
 }
+
+
